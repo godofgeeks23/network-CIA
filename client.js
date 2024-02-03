1bba6f0c971d2afb358135cf7fc8e9638e5b19f1
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-02-03 10:40:56 +0530

    secrets updated

--- client.js
+++ client.js
@@ -1,13 +1,17 @@
 const axios = require('axios');
 const crypto = require('crypto');
+// load secrets from env file
+require('dotenv').config()
 
-const secret = 'yourSecretKey'; // Use the same secret key as the server
+const secret = process.env.aes_secret; // Use the same secret key as the server
 const serverUrl = 'http://localhost:3000'; // Replace with your server URL
+console.log("secret - ", secret)
 
 // Dummy system information
 const systemInfo = {
   // Your system information here
-  ip: '117.34.16.8'
+  ip: "113.45.67.8",
+  mac: "0a:0f:0e:66:9d:00",
 };
 
 let secretKey = crypto.createHash('sha256').update(String(secret)).digest('base64').substr(0, 32);
@@ -21,7 +25,10 @@ encryptedData += cipher.final('hex');
 // Send encrypted data to the server for authentication
 axios.post(`${serverUrl}/authenticate`, { encryptedData, iv: iv.toString('hex') })
   .then(response => {
-    console.log(response.data.message);
+    const { token } = response.data;
+    console.log('Authentication successful. Token:', token);
+    // Save the token for subsequent requests
+    console.log("Sent encrypted data to Sever - ", encryptedData)
   })
   .catch(error => {
     console.error('Authentication failed:', error.message);
