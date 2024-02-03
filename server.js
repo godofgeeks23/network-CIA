222a199d637e06553404dfff935da81a1d9b940e
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-02-03 10:41:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+// load secrets from env file
+require('dotenv').config()
 
 const app = express();
-const secret = 'yourSecretKey'; // Replace with a strong secret key
+const secret = process.env.aes_secret // Replace with a strong secret key
+const jwtSecret = process.env.jwt_secret; // Replace with a strong JWT secret
 
 app.use(bodyParser.json());
 
@@ -11,17 +15,31 @@ app.use(bodyParser.json());
 app.post('/authenticate', (req, res) => {
   const { encryptedData, iv } = req.body;
 
+  console.log("Got encrpyted data from client - ", encryptedData)
+  console.log("secret - ", secret)
   let secretKey = crypto.createHash('sha256').update(String(secret)).digest('base64').substr(0, 32);
 
-  // Decrypt the data using AES
-  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secretKey), Buffer.from(iv, 'hex'));
-  let decryptedData = decipher.update(encryptedData, 'hex', 'utf-8');
-  decryptedData += decipher.final('utf-8');
+  try {
+    // Decrypt the data using AES
+    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secretKey), Buffer.from(iv, 'hex'));
+    let decryptedData = decipher.update(encryptedData, 'hex', 'utf-8');
+    decryptedData += decipher.final('utf-8');
+    console.log("Decrypted data - ", decryptedData)
+  }
+  catch (error) {
+    console.error('Decryption failed: ', error.message);
+    return res.status(500).json({ success: false, message: 'Decryption failed' });
+  }
 
   // Validate user or system information here
 
-  // Send response
-  res.json({ success: true, message: 'Authentication successful' });
+  // Generate JWT token
+  const token = jwt.sign({ userId: '123', /* additional claims */ }, jwtSecret, { expiresIn: '1h' });
+
+  
+
+  // Send the token as a response
+  res.json({ success: true, token });
 });
 
 const PORT = 3000;
